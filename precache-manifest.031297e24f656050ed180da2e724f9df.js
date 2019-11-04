fa10b043730aba7b4701038a2fd8889b1114b194
Farhan matar <farhan.alafi@gmail.com>
2019-11-04 12:29:31 +0100

    Updates

---
+++ precache-manifest.031297e24f656050ed180da2e724f9df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d83c96c00db90674e0c42ea4dae4e5b",
+    "url": "/farhan-website/index.html"
+  },
+  {
+    "revision": "d76a8114db9e0143d124",
+    "url": "/farhan-website/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f166ea6525418f3a75fb",
+    "url": "/farhan-website/static/js/2.75ef436d.chunk.js"
+  },
+  {
+    "revision": "d76a8114db9e0143d124",
+    "url": "/farhan-website/static/js/main.5108fa7a.chunk.js"
+  },
+  {
+    "revision": "f80758198315c3fcbe66",
+    "url": "/farhan-website/static/js/runtime-main.bf42d4be.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/farhan-website/static/media/logo.25bf045c.svg"
+  }
+]);
